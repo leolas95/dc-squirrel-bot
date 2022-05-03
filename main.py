882cf431f47c7a8c1f9e3fda3e95ef12f9565521
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2022-05-03 13:21:24 -0400

    minor readability improvement

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class SquirrelClient(discord.Client):
+    def sender_is_bot(self, message):
+      return message.author.id == self.user.id
+
     async def on_ready(self):
         print(f'Bot is ready as user {self.user}')
 
     async def on_message(self, message):
         # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
+        if self.sender_is_bot(message):
             return
         
         await message.add_reaction('🐿️')
